1d0190d6d64b06e4f5ef96369ef7f50bd04a3e71
gabriel <galhiardogabriel@gmail.com>
2025-09-19 17:01:43 -0300

    feat: adding arrow to open the card

--- app/src/main/java/com/example/classdiary/MainActivity.kt
+++ app/src/main/java/com/example/classdiary/MainActivity.kt
@@ -1,33 +1,59 @@
 package com.example.classdiary
 
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,56 +63,88 @@ import com.example.classdiary.data.DataSouce
 import com.example.classdiary.ui.theme.AbrilFatface
 import com.example.classdiary.ui.theme.ClassDiaryTheme
 import com.example.classdiary.ui.theme.Libertinus
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate called")
         enableEdgeToEdge()
         setContent {
             ClassDiaryTheme {
                 Scaffold(
-                    modifier =
-                        Modifier.fillMaxSize(),
-                    topBar = {
+                     topBar = {
                         ClassDiaryTopAppBar()
-                    }
-                )
-                { innerPadding ->
+                    }) { innerPadding ->
                     classDiaryApp(
                         modifier = Modifier
+                            .fillMaxSize()
+                            .statusBarsPadding()
                             .padding(innerPadding)
                     )
                 }
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ClassDiaryTopAppBar(modifier: Modifier = Modifier){
+fun ClassDiaryTopAppBar(modifier: Modifier = Modifier) {
     CenterAlignedTopAppBar(
         title = {
-                Text(
-                    text = "Lista de alunos")
-        },
-        modifier = Modifier
+            Text(
+                text = "Lista de alunos"
+            )
+        }, modifier = Modifier
     )
 }
 
 @Composable
 @Preview
 fun classDiaryApp(modifier: Modifier = Modifier) {
+    val layoutDirection = LocalLayoutDirection.current
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-            .padding(16.dp),
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateEndPadding(layoutDirection)
+            ),
 
         ) {
         StudentsList(
-            modifier = modifier,
-            DataSouce().loadAStudents()
+            modifier = modifier, DataSouce().loadAStudents()
         )
     }
 }
@@ -97,8 +155,7 @@ fun ClassDiaryPreviewDark() {
     ClassDiaryTheme(darkTheme = true) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             classDiaryApp(
-                modifier = Modifier
-                    .padding(innerPadding)
+                modifier = Modifier.padding(innerPadding)
             )
         }
     }
@@ -110,8 +167,7 @@ fun ClassDiaryPreview() {
     ClassDiaryTheme(darkTheme = false) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             classDiaryApp(
-                modifier = Modifier
-                    .padding(innerPadding)
+                modifier = Modifier.padding(innerPadding)
             )
         }
     }
@@ -119,8 +175,7 @@ fun ClassDiaryPreview() {
 
 @Composable
 fun StudentsList(
-    modifier: Modifier = Modifier,
-    studentList: List<Student>
+    modifier: Modifier = Modifier, studentList: List<Student>
 ) {
     LazyColumn(modifier = modifier) {
         items(studentList) { student ->
@@ -136,30 +191,71 @@ fun StudentsList(
 }
 
 @Composable
-fun StudentCard(
-    @DrawableRes studentPhoto: Int,
-    studentName: String,
-    studentCurse: String,
-    modifier: Modifier
+fun StudentDetailsButton(
+    expanded: Boolean, modifier: Modifier = Modifier, onClick: () -> Unit
 ) {
+    IconButton(
+        onClick = onClick, modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ExpandMore,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+@Composable
+fun StudentsDetails() {
+    Column (
+        modifier = Modifier
+        .height(80.dp)
+        .fillMaxWidth()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Center,
+
+    ){
+        Text(
+            text = "Nota: 100",
+            fontSize = 16.sp,
+            fontFamily = AbrilFatface
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(
+            text = "Faltas: 3",
+            fontSize = 16.sp,
+            fontFamily = AbrilFatface
+        )
+    }
+}
+
+@Composable
+fun StudentCard(
+    @DrawableRes studentPhoto: Int, studentName: String, studentCurse: String, modifier: Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp), // Padding local do card
+            .padding(10.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMediumLow
+                )
+            ),
         elevation = CardDefaults.cardElevation(8.dp),
         shape = RoundedCornerShape(
-            bottomStart = 26.dp,
-            topEnd = 26.dp,
-            bottomEnd = 1.dp,
-            topStart = 1.dp
+            bottomStart = 26.dp, topEnd = 26.dp, bottomEnd = 1.dp, topStart = 1.dp
+
         )
     ) {
         Row(
             modifier = Modifier
-                .padding(8.dp) // Padding interno
+                .padding(8.dp)
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
+            horizontalArrangement = Arrangement.Center
         ) {
             Image(
                 painter = painterResource(id = studentPhoto),
@@ -169,24 +265,36 @@ fun StudentCard(
                     .size(100.dp)
                     .padding(10.dp)
                     .clip(CircleShape)
+                    .weight(1f)
             )
 
             Column(
-                modifier = Modifier.padding(8.dp)
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(2f)
             ) {
                 Text(
-                    text = studentName,
-                    fontSize = 20.sp,
-                    fontFamily = AbrilFatface
+                    text = studentName, fontSize = 20.sp, fontFamily = Libertinus
                 )
                 Text(
-                    text = studentCurse,
-                    fontSize = 16.sp,
-                    fontFamily = Libertinus
+                    text = studentCurse, fontSize = 16.sp, fontFamily = AbrilFatface
                 )
             }
+            StudentDetailsButton(
+                modifier = modifier
+                    .weight(0.5f)
+                    .wrapContentSize(align = Alignment.BottomCenter),
+                expanded = expanded,
+                onClick = { expanded = !expanded})
+
+        }
+        if (expanded) {
+            StudentsDetails()
+
         }
     }
+
 }
+
 
 
--- app/src/main/java/com/example/classdiary/data/DataSouce.kt
+++ app/src/main/java/com/example/classdiary/data/DataSouce.kt
@@ -5,12 +5,10 @@ import com.example.classdiary.R
 class DataSouce {
     fun loadAStudents(): List<Student>{
         return  listOf<Student>(
-            Student("Gabriel", R.drawable.ic_launcher_background, "Desenvolvimento de sistemas"),
-            Student("Gabriel", R.drawable.ic_launcher_background, "Desenvolvimento de sistemas"),
-            Student("Gabriel", R.drawable.ic_launcher_background, "Desenvolvimento de sistemas"),
-            Student("Gabriel", R.drawable.ic_launcher_background, "Desenvolvimento de sistemas"),
-            Student("Gabriel", R.drawable.ic_launcher_background, "Desenvolvimento de sistemas"),
-            Student("Gabriel", R.drawable.ic_launcher_background, "Desenvolvimento de sistemas")
+            Student("Gabriel", R.drawable.gabriel,"Desenvolvimento de sistemas"),
+            Student("Igor", R.drawable.igor, "Desenvolvimento de sistemas"),
+            Student("Gustavo", R.drawable.gustavo, "Desenvolvimento de sistemas"),
+            Student("Eric", R.drawable.eric, "Desenvolvimento de sistemas")
         )
     }
 }
